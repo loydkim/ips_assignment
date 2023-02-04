ff1ca87048a0a9f9f0836ad5b89671bdd7b4a8d7
Loydkim <kimloydmail@gmail.com>
2023-02-04 08:03:43 -0800

    init project

---
+++ ips_assignment/ips_assignment/ips_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ips_assignmentApp.swift
+//  ips_assignment
+//
+//  Created by Loyd Kim on 2023-02-04.
+//
+
+import SwiftUI
+
+@main
+struct ips_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

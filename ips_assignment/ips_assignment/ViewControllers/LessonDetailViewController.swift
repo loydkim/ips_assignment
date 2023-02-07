4160de604296eff13d8f559fe83705893f9109ce
Loydkim <kimloydmail@gmail.com>
2023-02-06 18:25:32 -0800

    style  - Update name and description style of detail page  - Add play and next button

--- ips_assignment/ips_assignment/ViewControllers/LessonDetailViewController.swift
+++ ips_assignment/ips_assignment/ViewControllers/LessonDetailViewController.swift
@@ -23,6 +23,9 @@ struct LessonDetailViewControllerWrapper: UIViewControllerRepresentable{
 class LessonDetailViewController: UIViewController{
     var lesson: Lesson?
     
+//    let scrollView = UIScrollView()
+//    let contentView = UIView()
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         self.navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(self.downloadVideo(_:)))
@@ -34,6 +37,7 @@ class LessonDetailViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        setupScrollView()
         setChildViews()
     }
     
@@ -41,49 +45,96 @@ class LessonDetailViewController: UIViewController{
         print("Move to next Lesson")
     }
     
+    @objc func playMovie(sender : UIButton) {
+        print("Start moive")
+    }
+    
+//    func setupScrollView() {
+//        scrollView.translatesAutoresizingMaskIntoConstraints = false
+//        contentView.translatesAutoresizingMaskIntoConstraints = false
+//        self.view.addSubview(scrollView)
+//        scrollView.addSubview(contentView)
+//        scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        scrollView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+//        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+//        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+//        contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+//        contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+//        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+//        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+//    }
+    
     func setChildViews() {
         let thumbnailView: UIImageView = UIImageView()
+        thumbnailView.backgroundColor = .systemGray2
         self.view.addSubview(thumbnailView)
-        thumbnailView.backgroundColor = .red
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: URL(string: self!.lesson!.thumbnail)!) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        thumbnailView.image = image
+                    }
+                }
+            }
+        }
+        
         thumbnailView.translatesAutoresizingMaskIntoConstraints = false
         thumbnailView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
         thumbnailView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
         thumbnailView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1).isActive = true
         thumbnailView.heightAnchor.constraint(equalToConstant: 210).isActive = true
         
+        let playButton = UIButton()
+//        playButton.frame = CGRect(x: 0, y: 0, width: 350, height: 350)
+        playButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        playButton.tintColor = UIColor.white
+//        playButton.setTitle("Next Lesson >", for: .normal)
+//        playButton.setTitleColor(UIColor(red: 41/255, green: 101/255, blue: 250/255, alpha: 1.0), for: .normal)
+        playButton.addTarget(self, action: #selector(self.playMovie), for: .touchUpInside)
+        self.view.addSubview(playButton)
+        
+        playButton.translatesAutoresizingMaskIntoConstraints = false
+        playButton.centerXAnchor.constraint(equalTo: thumbnailView.centerXAnchor).isActive = true
+        playButton.centerYAnchor.constraint(equalTo: thumbnailView.centerYAnchor).isActive = true
+        
+        playButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        playButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
         let nameLabel: UILabel = UILabel()
         nameLabel.text = lesson?.name ?? "No name"
+        nameLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 26)
+        
         nameLabel.lineBreakMode = .byWordWrapping
-        nameLabel.numberOfLines = 3
+        nameLabel.numberOfLines = 0
         self.view.addSubview(nameLabel)
         
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
-        nameLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10).isActive = true
-        nameLabel.topAnchor.constraint(equalTo: thumbnailView.bottomAnchor, constant: 10).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 14).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: thumbnailView.bottomAnchor, constant: 20).isActive = true
         nameLabel.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8).isActive = true
         
         let descriptionLabel: UILabel = UILabel()
         descriptionLabel.text = lesson?.description ?? "No Description"
         descriptionLabel.lineBreakMode = .byWordWrapping
-        descriptionLabel.numberOfLines = 3
+        descriptionLabel.numberOfLines = 0
         self.view.addSubview(descriptionLabel)
         
         descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        descriptionLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10).isActive = true
-        descriptionLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 10).isActive = true
+        descriptionLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 14).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 20).isActive = true
         descriptionLabel.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8).isActive = true
         
-        let nextLesson = UIButton()
-        nextLesson.setTitle("Next Lesson >", for: .normal)
-        nextLesson.setTitleColor(UIColor(red: 41/255, green: 101/255, blue: 250/255, alpha: 1.0), for: .normal)
-        nextLesson.addTarget(self, action: #selector(self.nextLesson), for: .touchUpInside)
-        self.view.addSubview(nextLesson)
+        let nextLessonButton = UIButton()
+        nextLessonButton.setTitle("Next Lesson >", for: .normal)
+        nextLessonButton.setTitleColor(UIColor(red: 41/255, green: 101/255, blue: 250/255, alpha: 1.0), for: .normal)
+        nextLessonButton.addTarget(self, action: #selector(self.nextLesson), for: .touchUpInside)
+        self.view.addSubview(nextLessonButton)
         
-        nextLesson.translatesAutoresizingMaskIntoConstraints = false
-        nextLesson.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 10).isActive = true
-        nextLesson.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 10).isActive = true
-        nextLesson.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        nextLesson.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        nextLessonButton.translatesAutoresizingMaskIntoConstraints = false
+        nextLessonButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 10).isActive = true
+        nextLessonButton.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 10).isActive = true
+        nextLessonButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        nextLessonButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
         
     }
 }
